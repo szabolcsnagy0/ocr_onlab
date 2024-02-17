446568683a3537c798cba067f534932332222359
szabolcsnagy0 <n.szaby4@gmail.com>
2024-02-17 15:52:34 +0100

    Adat feldolgozása az elő- és hátlapon

--- IDDemo/Paddle.py
+++ IDDemo/Paddle.py
@@ -1,15 +1,30 @@
 from paddleocr import (PaddleOCR,
                        draw_ocr)
+import re
 
 
 class Person:
-    def __init__(self, name='', date_of_birth='', id=0):
-        self.name = name
-        self.date_of_birth = date_of_birth
-        self.id = id
+    def __init__(self):
+        self.date_of_expiry = None
+        self.date_of_expiry_confidence = 0
+        self.nationality = None
+        self.nationality_confidence = 0
+        self.sex = None
+        self.sex_confidence = 0
+        self.id_confidence = 0
+        self.date_of_birth_confidence = 0
+        self.name_confidence = 0
+        self.name = None
+        self.date_of_birth = None
+        self.id = None
 
     def print(self):
-        print("Name:\n\t", self.name, "\nDate of birth:\n\t", self.date_of_birth, "\nId number:\n\t", self.id)
+        print("Name:\n\t", self.name, " ", self.name_confidence,
+              "\nDate of birth:\n\t", self.date_of_birth, " ", self.date_of_birth_confidence,
+              "\nDocument number:\n\t", self.id, " ", self.id_confidence,
+              "\nDate of expiry:\n\t", self.date_of_expiry, " ", self.date_of_expiry_confidence,
+              "\nSex:\n\t", self.sex, " ", self.sex_confidence,
+              "\nNationality:\n\t", self.nationality, " ", self.nationality_confidence, "\n\n")
 
 
 def show_result(result, img_path):
@@ -27,7 +42,7 @@ def show_result(result, img_path):
     # im_show.save('result.jpg')
 
 
-def check_sum(string):
+def check_sum(string, check):
     weights = [7, 3, 1]
     weight_index = 0
     calculated_sum = 0
@@ -40,50 +55,132 @@ def check_sum(string):
                 value = value - 7
         calculated_sum = (value * weights[weight_index] + calculated_sum) % 10
         weight_index = (weight_index + 1) % 3
-    return calculated_sum
+    return calculated_sum == (ord(check) - ord('0'))
+
+
+def process_front(ocr_result):
+    person = Person()
+
+    # Extract data
+    # Name
+    person.name = ocr_result[3][1][0]
+    person.name_confidence = ocr_result[3][1][1]
+
+    # Sex
+    text = ocr_result[5][1][0]
+    person.sex_confidence = ocr_result[5][1][1]
+    male = ("M" in text or "FÉRFI" in text or "FERFI" in text) and ("NO" not in text and "NŐ" not in text)
+    female = ("F" in text or "NŐ" in text or "NO" in text) and (
+            "M" not in text and "FÉRFI" not in text and "FERFI" not in text)
+    if male and female:
+        person.sex_confidence = 0
+    else:
+        if male:
+            person.sex = "M"
+        else:
+            person.sex = "F"
+
+    # Nationality
+    text = ocr_result[6][1][0]
+    person.nationality = text[-3:]
+    person.nationality_confidence = ocr_result[6][1][1]
+
+    # Date of birth
+    text = ocr_result[8][1][0]
+    person.date_of_birth = text[-2:] + text[2:4] + text[:2]
+    person.date_of_birth_confidence = ocr_result[8][1][1]
+
+    # Document number
+    id1 = ocr_result[4][1][0]
+    id1_conf = ocr_result[4][1][1]
+    id2 = ocr_result[12][1][0]
+    id2_conf = ocr_result[12][1][1]
+    if id1_conf > id2_conf:
+        person.id = id1
+        person.id_confidence = id1_conf
+    else:
+        person.id = id2
+        person.id_confidence = id2_conf
+
+    # Expiry
+    text = ocr_result[10][1][0]
+    person.date_of_expiry = text[-2:] + text[2:4] + text[:2]
+    person.date_of_expiry_confidence = ocr_result[10][1][1]
+
+    return person
+
+
+def process_back(ocr_result):
+    person = Person()
+
+    # Process data
+    mrz_lines = ocr_result[-3:]
+
+    # Sex
+    text = mrz_lines[1][1][0]
+    person.sex = text[7]
+    person.sex_confidence = mrz_lines[1][1][1]
+
+    # Name
+    text = mrz_lines[2][1][0]
+    person.name = text.replace("<<", " ", 1).replace("<", "")
+    person.name_confidence = mrz_lines[2][1][1]
+
+    # Nationality
+    text = mrz_lines[1][1][0]
+    person.nationality = text[15:18]
+    person.nationality_confidence = mrz_lines[1][1][1]
+
+    # Checksum for the first two rows
+    lines_concat = mrz_lines[0][1][0] + re.sub('[^0-9]', '', mrz_lines[1][1][0])
+    text = lines_concat[5:-1]
+    if not check_sum(text, lines_concat[-1]):
+        print("ERROR: MRZ check digit not matching for the first two rows!\n", text)
+        return person
+
+    # Document number
+    text = mrz_lines[0][1][0]
+    id = text[5:13]
+    if check_sum(id, text[14]):
+        person.id = id
+        person.id_confidence = 1
+    else:
+        print("ERROR: MRZ check digit not matching for document number!\n", id)
+
+    # Date of birth
+    text = mrz_lines[1][1][0]
+    date_of_birth = text[0:6]
+    if check_sum(date_of_birth, text[6]):
+        person.date_of_birth = date_of_birth
+        person.date_of_birth_confidence = 1
+    else:
+        print("ERROR: MRZ check digit not matching for date of birth!\n", date_of_birth)
+
+    # Date of expiry
+    text = mrz_lines[1][1][0]
+    date_of_expiry = text[8:14]
+    if check_sum(date_of_expiry, text[14]):
+        person.date_of_expiry = date_of_expiry
+        person.date_of_expiry_confidence = 1
+    else:
+        print("ERROR: MRZ check digit not matching for date of expiry!\n", date_of_expiry)
+    return person
 
 
 paddle_ocr = PaddleOCR(use_angle_cls=True, lang='hu')
 
-img_path = '../images/mrp_id_crop.png'
-result = paddle_ocr.ocr(img_path, cls=True)
-show_result(result, img_path)
+# Front
+img_front = '../images/id_front.jpeg'
+result_front = paddle_ocr.ocr(img_front, cls=True)
+print("\n\n----------FRONT----------")
+process_front(result_front[0]).print()
 
-person = Person()
+# Back
+img_back = '../images/id_back.png'
+result_back = paddle_ocr.ocr(img_back, cls=True)
+print("\n\n----------BACK----------")
+process_back(result_back[0]).print()
 
-# Process data
-size = len(result[0])
-mrz_lines = result[0][size-3:size]
 
-# Checksum for the first two rows
-import re
-lines_concat = mrz_lines[0][1][0] + re.sub('[^0-9]', '', mrz_lines[1][1][0])
-text = lines_concat[5:-1]
-check = ord(lines_concat[-1])-ord('0')
-if check_sum(text) != check:
-    print("ERROR: MRZ first two rows! ", check_sum(text), check, "\n", text)
-
-# Obtain ID
-text = mrz_lines[0][1][0]
-id = text[5:13]
-check = ord(text[14]) - ord('0')
-
-if check_sum(id) == check:
-    person.id = id
-else:
-    print("INCORRECT ID: ", id, check_sum(id), check)
-
-# Obtain date of birth
-text = mrz_lines[1][1][0]
-date_of_birth = text[0:6]
-check = ord(text[6]) - ord('0')
-if check_sum(date_of_birth) == check:
-    person.date_of_birth = date_of_birth
-else:
-    print("INCORRECT DATE: ", date_of_birth, check_sum(date_of_birth), check)
-
-# Obtain name
-text = mrz_lines[2][1][0]
-person.name = text.replace("<<", " ", 1).replace("<", "")
-
-person.print()
+show_result(result_front, img_front)
+show_result(result_back, img_back)
