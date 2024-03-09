be586f5a303f22dc6ded6dc285744c52decfd3d6
szabolcsnagy0 <n.szaby4@gmail.com>
2024-03-09 14:20:28 +0100

    Modell tesztelése

--- IDDemo/Llama.py
+++ IDDemo/Llama.py
@@ -8,43 +8,57 @@ import io
 
 # # Langchain
 #
-# template = """
-# You are a linguist who knows all existing hungarian first and last names.
+# with io.open("../names/test.txt", mode="r", encoding="utf8") as f:
+#     words = f.read().splitlines()
 #
-# Question: Is {name} a grammatically correct hungarian first name? If not, correct it.
+# for word in words:
+#     prompt = """
+#     ###Human:
+#     Correct this hungarian first name: {word}
 #
-# Answer: """
+#     ###Assistant:
+#     """.format(word=word)
 #
-# callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+#     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 #
-# llm = LlamaCpp(
-#     model_path="D:/llama-2-7b-chat.Q2_K.gguf",
-#     temperature=0.1,
-#     max_tokens=2000,
-#     top_p=1,
-#     stop=["\n"],
-#     callback_manager=callback_manager,
-#     verbose=True,  # Verbose is required to pass to the callback manager
-#     grammar_path="../grammars/grammar.gbnf"
-# )
+#     llm = LlamaCpp(
+#         model_path="D:/top-names-13b-v1.5.gguf",
+#         temperature=0.1,
+#         max_tokens=2000,
+#         top_p=1,
+#         stop=["\n"],
+#         callback_manager=callback_manager,
+#         verbose=True,  # Verbose is required to pass to the callback manager
+#         # grammar_path="../grammars/grammar.gbnf"
+#     )
 #
-# prompt = template.format(name="Szabokcs")
-# # prompt = template.format(name="Nagy Szabolcs")
-# llm.invoke(prompt)
+#     llm.invoke(prompt)
 
-# # llama_cpp
-#
-# llm = Llama(
-#     model_path="D:/llama-2-7b-chat.Q2_K.gguf"
-# )
-# output = llm(
-#       "Q: Is Szabolds a grammatically correct hungarian first name? If not, what is the correct form? A: ", # Prompt
-#       max_tokens=50,
-#       stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
-#       echo=False, # Echo the prompt back in the output
-# ) # Generate a completion, can also call create_completion
-#
-# print(output['choices'][0]['text'])
+# llama_cpp
+
+llm = Llama(
+    model_path="D:/top-names-13b-v1.5.gguf"
+)
+
+# Read the words from the input file
+with io.open("../names/test.txt", mode="r", encoding="utf8") as f:
+    words = f.read().splitlines()
+
+for word in words:
+    prompt = """
+    ###Human:
+    Correct this hungarian first name: {word}
+
+    ###Assistant:
+    """.format(word=word)
+    output = llm(
+          prompt, # Prompt
+          max_tokens=200,
+          stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+          echo=True, # Echo the prompt back in the output
+    ) # Generate a completion, can also call create_completion
+
+    print(output['choices'][0]['text'])
 
 
 # Trying out specific grammar
@@ -69,33 +83,3 @@ import io
 #           "spelling errors or it might have missing data. Try correcting the errors you find and tell me "
 #           "what you know about the person!")
 # llm.invoke(prompt + "\n" + ocr_text)
-
-
-# Trained Model
-
-import torch
-from transformers import T5Model, AutoTokenizer, AutoModelForCausalLM
-
-MODEL_PATH = "../../top_names"
-ACCESS_TOKEN = "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"
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, token=ACCESS_TOKEN)
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL_PATH,
-    device_map="auto",
-    torch_dtype='auto'
-).eval()
-
-input_context = """
-###Human:
-Correct this hungarian first name: Szabojcs
-
-###Assistant:
-"""
-
-input_ids = tokenizer.encode(input_context, return_tensors="pt")
-output = model.generate(input_ids, max_length=85, temperature=0.3, num_return_sequences=1)
-
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-print(generated_text)
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Fullstack alapú modern karakter/dokumentum/kártya felismerő és üzleti folyamatokat támogató rendszer
 
 ## [Haladási napló](https://docs.google.com/document/d/1lzh5dGaKfHcXSdRQJbmSAakjnDAcitlRk6MZ9UpTBNw/edit?usp=sharing)
+
+## [Tesztelési eredmények](https://docs.google.com/spreadsheets/d/e/2PACX-1vQWEVFPyAE6keNLIgdWi1gqH8lvmcX0nwvyDr9XQcEXTIK_IvXs5K8g9bnLxZlcVHjkdKVngyDl63oE/pubhtml)
